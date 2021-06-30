c39a6cfa38203a6dc377bc40986b901e26743463
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-06-29 23:32:14 -0500

    added 5th day

--- js/script.js
+++ js/script.js
@@ -18,10 +18,14 @@ var fDTemp = document.getElementById('fourth-d-temp')
 var fDWind = document.getElementById('fourth-d-wind')
 var fDHum = document.getElementById('fourth-d-hum')
 var fDUvi = document.getElementById('fourth-d-uv')
-var lDTemp = document.getElementById('fifth-d-temp')
-var lDWind = document.getElementById('fifth-d-wind')
-var lDHum = document.getElementById('fifth-d-hum')
-var lDUvi = document.getElementById('fifth-d-uv')
+var fiDTemp = document.getElementById('fifth-d-temp')
+var fiDWind = document.getElementById('fifth-d-wind')
+var fiDHum = document.getElementById('fifth-d-hum')
+var fiDUvi = document.getElementById('fifth-d-uv')
+var sDTemp = document.getElementById('sixth-d-temp')
+var sDWind = document.getElementById('sixth-d-wind')
+var sDHum = document.getElementById('sixth-d-hum')
+var sDUvi = document.getElementById('sixth-d-uv')
 
 const APIKey = "6ebf25cc38abf50137f1b884c40624f2";
 const click = document.getElementById('click-me');
@@ -60,10 +64,14 @@ fetchButton.addEventListener('click', function(){
                 fDWind.textContent = "Wind: " + data.daily[3].wind_speed + " MPH";
                 fDHum.textContent = "Humidity: " + data.daily[3].humidity + " %";
                 fDUvi.textContent = "Uv Index: " + data.daily[3].uvi;
-                lDTemp.textContent = "Temp: " + data.daily[4].temp.day + " °F";
-                lDWind.textContent = "Wind: " + data.daily[4].wind_speed + " MPH";
-                lDHum.textContent = "Humidity: " + data.daily[4].humidity + " %";
-                lDUvi.textContent = "Uv Index: " + data.daily[4].uvi;
+                fiDTemp.textContent = "Temp: " + data.daily[4].temp.day + " °F";
+                fiDWind.textContent = "Wind: " + data.daily[4].wind_speed + " MPH";
+                fiDHum.textContent = "Humidity: " + data.daily[4].humidity + " %";
+                fiDUvi.textContent = "Uv Index: " + data.daily[4].uvi;
+                sDTemp.textContent = "Temp: " + data.daily[5].temp.day + " °F";
+                sDWind.textContent = "Wind: " + data.daily[5].wind_speed + " MPH";
+                sDHum.textContent = "Humidity: " + data.daily[5].humidity + " %";
+                sDUvi.textContent = "Uv Index: " + data.daily[5].uvi;
             })
 
         })
